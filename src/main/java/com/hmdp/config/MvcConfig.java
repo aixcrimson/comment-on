beec8d6ab482ff8d7699b1f75985cd5ae8272dcf
GotohHitori <hitori150221@outlook.com>
2024-05-30 22:40:25 +0800

    登录拦截器实现

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 登录拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "shop-type/**",
+                        "/upload/**",
+                        "blog/hot",
+                        "user/code",
+                        "/user/login");
+    }
+}
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取session
+        HttpSession session = request.getSession();
+        // 2.获取session中的用户
+        Object user = session.getAttribute("user");
+        // 3.判断用户是否存在
+        if(user == null) {
+            // 4.不存在，拦截，设置状态码401
+            response.setStatus(401);
+            return false;
+        }
+        // 5.存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser((UserDTO) user);
+        // 6.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 移除用户，清空thread local的信息，第一可以做到退出登录，第二可以防止内存泄露
+        UserHolder.removeUser();
+    }
+}
