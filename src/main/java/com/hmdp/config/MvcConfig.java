a405af9763b1f0969b69244dd4bd466d53dedd45
GotohHitori <hitori150221@outlook.com>
2024-05-31 15:55:29 +0800

    隐藏用户敏感信息并返回

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     /**
@@ -17,8 +19,8 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/shop/**",
                         "shop-type/**",
                         "/upload/**",
-                        "blog/hot",
-                        "user/code",
+                        "/blog/hot",
+                        "/user/code",
                         "/user/login");
     }
 }
