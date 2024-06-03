dc82c46b3960c54790096552ea57205cb8a96b9f
GotohHitori <hitori150221@outlook.com>
2024-06-03 22:41:50 +0800

    解决商铺查询的缓存穿透问题(缓存空对象)

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -16,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -49,11 +48,17 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-        // 3.未命中，根据id查询数据库
+        // 3.判断命中的是否是空值
+        if(shopJson != null){
+            // 返回错误信息
+            return Result.fail("店铺不存在！");
+        }
+        // 4.未命中，根据id查询数据库
         Shop shop = getById(id);
-        // 4.判断是否存在
+        // 判断是否存在
         if(shop == null){
-            // 5.不存在，返回错误信息
+            // 5.不存在，将空置写入Redis，返回错误信息
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
         // 6.存在，写入Redis
