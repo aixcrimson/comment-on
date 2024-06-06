493758cb2c9492fd3d2de4a4a8ead9d766e47443
GotohHitori <hitori150221@outlook.com>
2024-06-06 21:36:51 +0800

    乐观锁解决秒杀券超卖问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -55,8 +55,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
         // 5.扣减库存
         boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .setSql("stock = stock - 1") // set stock = stock - 1
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0) // where id = ? and stock > 0
+                .update();
         if(!success){
             // 库存不足
             return Result.fail("库存不足！");
