d4e5e24d1ccc5fb2fe911c4e2283e8dedabb2a11
GotohHitori <hitori150221@outlook.com>
2024-06-06 22:51:33 +0800

    解决一人一单问题

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -15,4 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
     Result sekillVocher(Long voucherId);
+    Result createVoucherOrder(Long voucherId);
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -2,7 +2,6 @@ package com.hmdp.service.impl;
 
 import com.hmdp.dto.Result;
 import com.hmdp.entity.SeckillVoucher;
-import com.hmdp.entity.Voucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
@@ -10,15 +9,16 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -34,6 +34,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     /**
      * 优惠券秒杀下单
+     *
      * @param voucherId
      * @return
      */
@@ -42,39 +43,67 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         // 1.查询优惠券信息
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
         // 2.判断秒杀是否开始
-        if(voucher.getBeginTime().isAfter(LocalDateTime.now())){
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
             return Result.fail("秒杀还未开始！");
         }
         // 3.判断秒杀是否已经结束
-        if(voucher.getEndTime().isBefore(LocalDateTime.now())){
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
             return Result.fail("秒杀已经结束！");
         }
         // 4.判断库存是否充足
-        if(voucher.getStock() < 1){
+        if (voucher.getStock() < 1) {
             return Result.fail("秒杀券库存不足！");
         }
-        // 5.扣减库存
+
+        Long userId = UserHolder.getUser().getId();
+        // 保证事务的特性，同时控制锁的粒度
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象,(事务想要生效，还得利用代理来生效，我们需要获得原始的事务对象，来操作事务)
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    /**
+     * 封装方法实现一人一单
+     *
+     * @param voucherId
+     * @return
+     */
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 5.一人一单
+        // 5.1.用户id
+        Long userId = UserHolder.getUser().getId();
+        // 5.2.查询订单
+        Integer count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 5.3.判断是否存在
+        if (count > 0) {
+            // 用户已经购买过了
+            return Result.fail("用户已经购买过了！");
+        }
+        // 6.扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1") // set stock = stock - 1
                 .eq("voucher_id", voucherId)
                 .gt("stock", 0) // where id = ? and stock > 0
                 .update();
-        if(!success){
+        if (!success) {
             // 库存不足
             return Result.fail("库存不足！");
         }
-        // 6.创建订单
+        // 7.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        // 6.1.订单id
+        // 7.1.订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        // 6.2.用户id
-        Long userId = UserHolder.getUser().getId();
+        // 7.2.用户id
         voucherOrder.setUserId(userId);
-        // 6.3.秒杀券id
+        // 7.3.秒杀券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
-
+        // 8.返回订单id
         return Result.ok(orderId);
     }
+
 }
