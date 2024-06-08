27e801fba3a2a9f8275c70c83e2a17c6258a9c84
GotohHitori <hitori150221@outlook.com>
2024-06-08 16:50:15 +0800

    使用调用Lua脚本保证拿锁，比锁，删锁的原子性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.support.collections.DefaultRedisList;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -11,6 +15,13 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String lockName, StringRedisTemplate stringRedisTemplate) {
         this.lockName = lockName;
@@ -29,14 +40,19 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        // 获取线程标识
+        /*// 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的标识
         String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + lockName);
         if (threadId.equals(id)) {
             // 释放锁完成
             stringRedisTemplate.delete(KEY_PREFIX + lockName);
-        }
-
+        }*/
+        // 调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + lockName),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
     }
 }
