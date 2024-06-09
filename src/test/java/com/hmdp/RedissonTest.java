23e3b2bbab784f521bb099a5a21ea20d408e2944
GotohHitori <hitori150221@outlook.com>
2024-06-09 14:15:53 +0800

    Redisson可重入锁测试

--- src/test/java/com/hmdp/RedissonTest.java
+++ src/test/java/com/hmdp/RedissonTest.java
@@ -1,6 +1,7 @@
 package com.hmdp;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -15,9 +16,14 @@ import java.util.concurrent.TimeUnit;
 public class RedissonTest {
 
     @Resource
-    private RedissonClient redisson;
-    @Autowired
     private RedissonClient redissonClient;
+
+    private RLock lock;
+
+    @BeforeEach
+    void setUp() {
+        lock = redissonClient.getLock("order");
+    }
 
     /**
      * 测试redisson
@@ -37,6 +43,43 @@ public class RedissonTest {
                 // 释放锁
                 lock.unlock();
             }
+        }
+    }
+
+    /**
+     * 测试可重入锁
+     */
+    @Test
+    void method1(){
+        // 尝试获取锁
+        boolean isLock = lock.tryLock();
+        if(!isLock){
+            log.error("获取锁失败....1");
+            return;
+        }
+        try{
+            log.info("获取锁成功....1");
+            method2();
+            log.info("开始执行业务...1");
+        }finally {
+            log.warn("准备释放锁....1");
+            lock.unlock();
+        }
+    }
+
+    void method2(){
+        // 尝试获取锁
+        boolean isLock = lock.tryLock();
+        if(!isLock){
+            log.error("获取锁失败....2");
+            return;
+        }
+        try{
+            log.info("获取锁成功....2");
+            log.info("开始执行业务...2");
+        }finally {
+            log.warn("准备释放锁....2");
+            lock.unlock();
         }
     }
 }
